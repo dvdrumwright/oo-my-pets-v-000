313fe6e99a060b42dd3d76b957fd7fbb5511d36c
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-06-12 14:36:15 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -26,6 +26,33 @@ end
 def say_species 
    "I am a #{species}."
 end 
+
+def buy_fish(name)
+  @pets[:fishes] << Fish.new(name)
+end 
+
+def buy_cat(name)
+  @pets[:cats] << Cat.new(name)
+end 
   
+def buy_dog(name)
+  @pets[:dogs] << Dog.new(name)
+end 
+
+def walk_dogs
+  @pets[:dogs].each do |dog| 
+    dog.mood = "happy"
+end 
+end
+
+def play_with_cats 
+  @pets[:cats].each do |cats| 
+    cats.mood = "happy"
+end 
+end
+
+def feed_fish 
+end 
+
 
 end
